063c48975ed601a8e86396534531e4cc236d5476
Rennanlp <155587174+Rennanlp@users.noreply.github.com>
2024-01-26 13:52:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,65 @@
 # app.py
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, redirect, session, url_for
 from werkzeug.utils import secure_filename
 from unidecode import unidecode
 import os
 import csv
-from io import BytesIO
 from datetime import datetime
+from functools import wraps
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY') or b'_5#y2L"F4Q8z\n\xec]/'
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'csv'}
+
+# banco de usuários
+user_database = {
+    'conexao.premium': {
+        'password': 'senha123',
+        'name': 'Conexão Premium'
+    },
+    'Renan10': {
+        'password': '162593',
+        'name': 'Renan'
+    }
+}
+
+# verificar se o usuário está logado
+def login_required(view):
+    @wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        return view(*args, **kwargs)
+    return wrapped_view
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/')
+@login_required
 def index():
-    return render_template('index.html')
+    username = user_database.get(session.get('username', ''), {}).get('name', 'Convidado')
+    return render_template('index.html', username=username)
+
+# Página de login
+@app.route('/login')
+def login1():
+    if 'username' in session:
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+# autenticar o login
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')  # pegar o campo 'username'
+    password = request.form.get('senha')
+
+    if username in user_database and user_database[username]['password'] == password:
+        session['username'] = username  # armazenar o nome de usuário na sessão
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html', error='Usuário ou senha incorretos, tente novamente.')
 
 @app.route('/remove_accent', methods=['POST'])
 def remove_accent():
@@ -54,6 +97,11 @@ def remove_accent():
             return "Erro durante o processamento do arquivo: {}".format(str(e))
 
     return "Tipo de arquivo não permitido."
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
