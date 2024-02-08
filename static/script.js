40f34331d316dfc118583a196d285b94652b5830
Rennanlp <155587174+Rennanlp@users.noreply.github.com>
2024-02-08 17:32:29 -0300

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -132,3 +132,43 @@ async function searchTracking() {
         }
     }, loadingTime);
 }
+function removeTask(buttonElement) {
+    // Obtém o elemento pai li da tarefa a ser removida
+    var listItem = buttonElement.closest('.checkbox-wrapper');
+
+    // Remove o elemento li da lista
+    listItem.remove();
+}
+function addTask() {
+    // Obtenha o valor da nova tarefa do input
+    var newTaskValue = document.getElementById('task').value;
+
+    // Verifique se o valor não está vazio antes de adicionar a nova tarefa
+    if (newTaskValue.trim() !== '') {
+        // Crie um identificador único para a nova tarefa
+        var taskId = 'task_' + Date.now();
+
+        // Crie um novo elemento li para a nova tarefa
+        var newTaskElement = document.createElement('li');
+        newTaskElement.classList.add('checkbox-wrapper');
+        newTaskElement.innerHTML = `
+            <div class="checkbox-input">
+                <input type="checkbox" id="${taskId}" name="tasks" value="${newTaskValue}" onchange="toggleObservationInput(this)">
+                <label for="${taskId}"></label>
+            </div>
+            <span>${newTaskValue}</span>
+            <input type="text" class="observation-input" name="observations[]" placeholder="Observações">
+            <button type="button" class="remove-task" onclick="removeTask(this)">
+            <svg class="remove-svgIcon" viewBox="0 0 448 512">
+                <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+              </svg>
+</button>
+        `;
+
+        // Adicione o novo elemento li à lista de tarefas
+        document.querySelector('.list-container ul').appendChild(newTaskElement);
+
+        // Limpe o valor do input após adicionar a tarefa
+        document.getElementById('task').value = '';
+    }
+}
